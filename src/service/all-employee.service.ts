49622a85b71c145ba8f6806e148e8fb484e231d7
ElayatNisrine <nissrineelayat@gmail.com>
2021-06-08 12:12:28 +0100

    1 file modified

--- src/service/all-employee.service.ts
+++ src/service/all-employee.service.ts
@@ -1,6 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { from } from 'rxjs';
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 @Injectable({
   providedIn: 'root'
 })
